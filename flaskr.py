2889ac010211e5cddd29153b7777760392512599
TarCode <TarCode@github.com>
2015-06-19 15:57:38 +0200

    fixed syntax error on line 22

--- flaskr.py
+++ flaskr.py
@@ -19,7 +19,7 @@ app.config.from_object(__name__)
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
 
-def init_db()"
+def init_db():
 	with closing(connect_db()) as db:
 		with app.open_resource('schema.sql', mode='r') as f:
 			db.cursor().executescript(f.read())
