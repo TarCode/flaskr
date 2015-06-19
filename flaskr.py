35426c476e05c1af9160561029a62a864412c723
TarCode <TarCode@github.com>
2015-06-19 14:00:03 +0200

    added db connection and show entries function

--- flaskr.py
+++ flaskr.py
@@ -1,5 +1,6 @@
 #imports
 import sqlite3
+from contextlib import closing
 from flask import Flask, request, session, g, redirect, url_for, \
 	abort, render_template, flash
 
@@ -17,6 +18,27 @@ app.config.from_object(__name__)
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
 
+def init_db()"
+	with closing(connect_db()) as db:
+		with app.open_resource('schema.sql', mode='r') as f:
+			db.cursor().executescript(f.read())
+		db.commit()
+
+@app.before_request
+def before_request():
+	g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+	db = getattr(g, 'db', None)
+	if db is not None:
+		db.close()
+
+@app.route('/')
+def show_entries():
+	cur = g.db.execute('select title, text from entries order by id desc')
+	entries = [dict(title=row[0], text=row[1]), for row in cur.fetchall()]
+	return render_template('show_entries.html', entries=entries)
 
 if __name__ == '__main__':
 	app.run()
